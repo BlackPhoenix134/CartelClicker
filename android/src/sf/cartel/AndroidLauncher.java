d33820065899dedcfb6050562a3438fbfcf64abf
BlackPhoenix134 <fabian.hermann@hotmail.com>
2021-05-09 20:59:45 +0200

    initial commit

---
+++ android/src/sf/cartel/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package sf.cartel;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import sf.cartel.CartelClickerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CartelClickerGame(), config);
+	}
+}
